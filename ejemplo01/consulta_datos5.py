cb10d00002a76f6f4457e6b5e6f3956376d68fd1
Omer21200 <omerbenitez212000@gmail.com>
2025-05-20 09:06:10 -0500

    consulta de clubs y jugador

--- ejemplo01/consulta_datos5.py
+++ ejemplo01/consulta_datos5.py
@@ -27,14 +27,14 @@ session = Session()
 
 clubs = session.query(Club).join(Jugador).\
         filter(Jugador.nombre.like("%Da%")).all()
-# print(clubs)
+print(clubs)
 print("Consulta 1 ")
 """
 Consulta 1 
 Club: nombre=Barcelona deporte=Fútbol fundación=1920
 """
-# for e in clubs: 
-#    print(e) 
+for e in clubs: 
+    print(e) 
 
 # Obtener un listado de todos los registros 
 # de la tabla Club y Jugador, que tengan al menos 
@@ -49,26 +49,6 @@ registros = session.query(Club, Jugador).join(Jugador).\
          filter(Jugador.nombre.like("%Da%")).all()
  
 print("Consulta 2 ")
-# print(registros)
-"""
-
-Consulta 2 
-
-Club: nombre=Barcelona deporte=Fútbol fundación=1920
-Jugador: Damian Diaz - dorsal:10 - posición: mediocampo
+print(registros)
 
 
-Club: nombre=Barcelona deporte=Fútbol fundación=1920
-Jugador: Dario Aymar - dorsal:2 - posición: defensa
-"""
-for registro in registros: 
-#     # el registro continen 
-#     # dos valores en un tupla
-#     # posición 0 el club
-#     # posición 1 el jugador 
-#     # que cumplen con la condición
-    print(registro[0].nombre) # El club
-    print(registro[1].nombre) # El jugador
-    print("------------------")
-
-# se cierra la sesión
